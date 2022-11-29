5629799e0559fd2b8e15c185fd2a97a74b65140b
Tomi Prasetyo <tomi.prasetyo95@gmail.com>
2022-11-29 19:23:53 +0700

    initial and setup project quote> setup splash screen

---
+++ android/app/src/main/kotlin/com/tomiprasetyo/flutter_movie_db/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tomiprasetyo.flutter_movie_db
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app_constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String imageUrlW500 = "https://image.tmdb.org/t/p/w500";
+  static const String imageUrlOriginal = "https://image.tmdb.org/t/p/original";
+  static const String apiKey = "276faae944cb51501e68f44aaf29f597";
+}
